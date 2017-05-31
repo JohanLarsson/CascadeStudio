2785a76290277d92c8c342c0717bc77683fd5f45
JohanLarsson <johan_gavle1@hotmail.com>
2017-05-31 12:52:32 +0200

    Use selector for main content.

---
+++ CascadeStudio/ProjectNodeTemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿namespace CascadeStudio
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class ProjectNodeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate PositivesDirectoryTemplate { get; set; }
+
+        public DataTemplate PositiveTemplate { get; set; }
+
+        public DataTemplate NegativesDirectoryTemplate { get; set; }
+
+        public DataTemplate ImageTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is PositiveViewModel)
+            {
+                return this.PositiveTemplate;
+            }
+
+            if (item is PositivesDirectory)
+            {
+                return this.PositivesDirectoryTemplate;
+            }
+
+            if (item is NegativeViewModel)
+            {
+                return this.ImageTemplate;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}

1e8bf69b0b2e052af234cfb11850b2fbf591b710
JohanLarsson <johan_gavle1@hotmail.com>
2017-06-04 11:03:02 +0200

    Tooltips.

--- CascadeStudio/Converters/StringToSizeConverter.cs
+++ CascadeStudio/Converters/StringToSizeConverter.cs
@@ -8,7 +8,7 @@ namespace CascadeStudio
     public class StringToSizeConverter : IValueConverter
     {
         public static readonly StringToSizeConverter Default = new StringToSizeConverter();
-        private static readonly char[] Separators = new[] { ',', ' ' };
+        private static readonly char[] Separators = { ',', ' ' };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -30,9 +30,15 @@ namespace CascadeStudio
                 }
 
                 var parts = text.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 1 &&
+                    double.TryParse(parts[0], NumberStyles.Any, CultureInfo.InvariantCulture, out double size))
+                {
+                    return new Size(size, size);
+                }
+
                 if (parts.Length == 2 &&
                     double.TryParse(parts[0], out double width) &&
-                    double.TryParse(parts[0], out double height))
+                    double.TryParse(parts[1], out double height))
                 {
                     return new Size(width, height);
                 }
